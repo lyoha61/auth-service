2984bd195bfa1e73cc306072aac7f2b06676ae33
GoodKid <nightmessenger703@gmail.com>
2025-07-27 21:46:49 +0300

    feat: change response data register method and wrapped in try/catch

--- src/controllers/AuthController.js
+++ src/controllers/AuthController.js
@@ -4,22 +4,31 @@ import bcrypt from 'bcrypt';
 export default class AuthController {
 
 	async register(req, res) {
-		const { name, email, password } = req.body;
-
-		const exists = await User.findOne({ email });
-
-		if (exists) {
-			return res.status(409).json({ 
-				error: 'Пользователь с таким email уже существует' 
-			});
+		try {
+			const { name, email, password } = req.body;
+	
+			const exists = await User.findOne({ email });
+	
+			if (exists) {
+				return res.status(409).json({ 
+					error: 'Пользователь с таким email уже существует' 
+				});
+			}
+	
+			const hashedPassword = await bcrypt.hash(password, 10);
+	
+			const user = new User({ name, email, password: hashedPassword });
+	
+			await user.save();
+	
+			const { _id: userId, name: userName, email: userEmail} = user.toObject();
+			
+			const userData = { id: userId, name: userName, email: userEmail }
+	
+			return res.status(201).json({message: 'Пользователь создан', userData});
+		} catch (err) {
+			console.error('Ошибка регистрации: ', err);
+			return res.status(500).json({ error: 'Ошибка при регистрации' });
 		}
-
-		const hashedPassword = await bcrypt.hash(password, 10);
-
-		const user = new User({ name, email, password: hashedPassword });
-
-		await user.save();
-
-		return res.status(201).json({message: 'Пользователь создан'}, user);
 	}
 }
