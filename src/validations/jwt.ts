622045006d5b5f5584af86ff82ab651d090fed99
lyoshka61 <nightmessenger703@gmail.com>
2025-08-03 15:13:38 +0300

    refactor: config docker enviroment

--- src/validations/jwt.ts
+++ src/validations/jwt.ts
@@ -2,5 +2,7 @@ import Joi from "joi";
 
 export const jwtPayloadSchema = Joi.object({
 	id: Joi.string().required(),
-	token_id: Joi.string().required()
+	token_id: Joi.string().required(),
+	iat: Joi.number().required(),
+	// exp: Joi.number().required(),
 })
